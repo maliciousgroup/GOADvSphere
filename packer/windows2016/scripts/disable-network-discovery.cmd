0ad8740451659eb30e2513ba597a00be14e72c90
d3d <info@malicious.group>
2023-12-19 15:33:35 -0500

    Delete packer directory

--- packer/windows2016/scripts/disable-network-discovery.cmd
+++
@@ -1,5 +0,0 @@
-rem Custom networking rules
-rem reg ADD HKLM\SYSTEM\CurrentControlSet\Control\Network\NewNetworkWindowOff /f
-rem netsh advfirewall firewall set rule group="Network Discovery" new enable=Yes
-rem netsh advfirewall firewall set rule group="File and Printer Sharing" new enable=Yes
-rem netsh advfirewall set privateprofile state on
