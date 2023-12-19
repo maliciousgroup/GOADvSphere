c3db9a322c029e62034880828eae8360ba4357b2
d3d <info@malicious.group>
2023-12-19 15:44:53 -0500

    Add files via upload

---
+++ terraform/build_templates/data.tf
@@ -0,0 +1,11 @@
+data "vsphere_datacenter" "dc" {
+  name = var.vsphere_datacenter
+}
+data "vsphere_datastore" "datastore" {
+  name          = var.vsphere_datastore
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
+data "vsphere_host" "esxi" {
+  name          = var.vsphere_esxi_host
+  datacenter_id = data.vsphere_datacenter.dc.id
+}
